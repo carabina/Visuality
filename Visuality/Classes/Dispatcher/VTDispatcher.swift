f9a757d883240537119fecfa64bc686e24ce8120
Igor Matyushkin <igormatyushkin014@gmail.com>
2015-09-24 15:18:59 +0300

    Added VTDispatcher class

--- Visuality/Classes/Dispatcher/VTDispatcher.swift
+++ Visuality/Classes/Dispatcher/VTDispatcher.swift
@@ -8,6 +8,58 @@
 
 import UIKit
 
-class VTDispatcher: NSObject {
-
+public class VTDispatcher: NSObject {
+    
+    // MARK: Class variables & properties
+    
+    
+    // MARK: Class methods
+    
+    public class func dispatchOnQueue(queue: dispatch_queue_t, afterTimeInterval timeInterval: NSTimeInterval, withBlock block: () -> Void) {
+        // Retrieve delay time
+        
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(timeInterval * Double(NSEC_PER_SEC)))
+        
+        
+        // Start dispatch
+        
+        dispatch_after(delayTime, queue) { () -> Void in
+            block()
+        }
+    }
+    
+    public class func dispatchOnMainQueueAfterTimeInterval(timeInterval: NSTimeInterval, withBlock block: () -> Void) {
+        // Retrieve queue
+        
+        let queue = dispatch_get_main_queue()
+        
+        
+        // Start dispatch
+        
+        dispatchOnQueue(queue, afterTimeInterval: timeInterval) { () -> Void in
+            block()
+        }
+    }
+    
+    
+    // MARK: Initializers
+    
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    
+    // MARK: Variables & properties
+    
+    
+    // MARK: Public methods
+    
+    
+    // MARK: Private methods
+    
+    
+    // MARK: Protocol methods
+    
 }
