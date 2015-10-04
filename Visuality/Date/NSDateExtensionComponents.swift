758b21ff7ebdf564207a8cfaba94f4fb543853b4
Igor Matyushkin <igormatyushkin014@gmail.com>
2015-10-05 00:49:14 +0300

    Changes for NSDate extension

--- Visuality/Date/NSDateExtensionComponents.swift
+++ Visuality/Date/NSDateExtensionComponents.swift
@@ -19,49 +19,43 @@ public extension NSDate {
     // MARK: Public object methods
     
     public func VT_seconds() -> Int {
-        let currentDate = NSDate()
         let calendar = NSCalendar.currentCalendar()
-        let componentsForCurrentDate = calendar.components(.Second, fromDate: currentDate)
+        let componentsForCurrentDate = calendar.components(.Second, fromDate: self)
         let currentSeconds = componentsForCurrentDate.second
         return currentSeconds
     }
     
     public func VT_minutes() -> Int {
-        let currentDate = NSDate()
         let calendar = NSCalendar.currentCalendar()
-        let componentsForCurrentDate = calendar.components(.Minute, fromDate: currentDate)
+        let componentsForCurrentDate = calendar.components(.Minute, fromDate: self)
         let currentMinutes = componentsForCurrentDate.minute
         return currentMinutes
     }
     
     public func VT_hours() -> Int {
-        let currentDate = NSDate()
         let calendar = NSCalendar.currentCalendar()
-        let componentsForCurrentDate = calendar.components(.Hour, fromDate: currentDate)
+        let componentsForCurrentDate = calendar.components(.Hour, fromDate: self)
         let currentHours = componentsForCurrentDate.hour
         return currentHours
     }
     
     public func VT_day() -> Int {
-        let currentDate = NSDate()
         let calendar = NSCalendar.currentCalendar()
-        let componentsForCurrentDate = calendar.components(.Day, fromDate: currentDate)
+        let componentsForCurrentDate = calendar.components(.Day, fromDate: self)
         let currentDay = componentsForCurrentDate.day
         return currentDay
     }
     
     public func VT_month() -> Int {
-        let currentDate = NSDate()
         let calendar = NSCalendar.currentCalendar()
-        let componentsForCurrentDate = calendar.components(.Month, fromDate: currentDate)
+        let componentsForCurrentDate = calendar.components(.Month, fromDate: self)
         let currentMonth = componentsForCurrentDate.month
         return currentMonth
     }
     
     public func VT_year() -> Int {
-        let currentDate = NSDate()
         let calendar = NSCalendar.currentCalendar()
-        let componentsForCurrentDate = calendar.components(.Year, fromDate: currentDate)
+        let componentsForCurrentDate = calendar.components(.Year, fromDate: self)
         let currentYear = componentsForCurrentDate.year
         return currentYear
     }
